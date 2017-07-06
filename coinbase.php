5c3c84bec46a5f73967b1d594c7a13b71df62ecd
Luke Dashjr <luke-jr+git+jun@utopios.org>
2017-07-06 23:05:19 +0000

    WIP

--- coinbase.php
+++ coinbase.php
@@ -1,5 +1,7 @@
 <?php
 
+require('kycpoll.php');
+
 function myerr($msg) {
 	echo("<html><head><title>Error</title><body>$msg</body></html>");
 	die;
@@ -55,78 +57,23 @@ if (!isset($response['result']['data']['id'])) {
 	myerr("Failed to get Coinbase id; <a href='?retry'>Click here to retry</a>");
 }
 
+$userdata = array(
+	'uuid' => 'coinbase_' . $response['result']['data']['id'],
+	'coinbase_userdata' => $response['result'],
+);
+
+$response = $client->fetch("https://api.coinbase.com/v2/payment-methods");
+$userdata['coinbase_payment_methods'] = $response['result'];
+
 echo('<html><head><title>KYCPoll</title></head><body>');
 echo("Hello ".$response['result']['data']['name']."<br>");
 echo('<br>');
 
-echo('<form method="POST">');
+echo('This data will be saved with your poll results. If there is too much personal information included, please contact luke-jr to improve the filtering <em>before</em> filling out the poll.')
+echo('<pre>' . json_encode($userdata) . '</pre>');
 
-function mypoll($id, $title, $options) {
-	echo("<h2>$title</h2>");
-	foreach ($options as $val => $desc) {
-		$ischecked = '';
-		// TODO: look up current selection
-		echo("<input type='radio' name='$id' value='$val'$ischecked> $desc<br>");
-	}
-}
+echo('Note that if you have not completed KYC with Coinbase, your results will be saved but ignored until you complete KYC <em>and</em> resubmit your poll results.');
 
-mypoll('segwit', 'Segwit', array(
-	'unconditional' => 'I support Segwit',
-	'compromise' => 'I consent to Segwit only bundled with a hardfork',
-	'oppose' => 'I oppose Segwit',
-));
+polls();
 
-mypoll('bip148', 'BIP148', array(
-	'unconditional' => 'I unconditionally support BIP148',
-	'conditional' => 'I support BIP148 only if <input size="3" name="bip148_pct">% of users also support it',
-	'oppose' => 'I oppose BIP148',
-));
-
-mypoll('blocksizehf', 'Block size hardfork', array(
-	'unconditional' => 'I support a hardfork to increase block size',
-	'compromise' => 'I consent to a block size increase hardfork only if bundled with Segwit',
-	'oppose' => 'I oppose a block size increase hardfork',
-));
-
-
-    $response = $client->fetch("https://api.coinbase.com/v2/payment-methods");
-
-//     echo('<strong>Response for fetch payment-methods:</strong><pre>');
-//     print_r($response);
-//     echo('</pre>');
-    $found_bank = false;
-    $has_kyc = null;
-    $foreign_currency = null;
-    foreach ($response['result']['data'] as $acct) {
-		if (!preg_match('/^(.*_bank_account|bank_wire|_card|interac)$/', $acct['type'])) {
-			continue;
-		}
-		$found_bank = true;
-		$buy_limit = $acct['limits']['buy'][0];
-		foreach ($buy_limit['next_requirement'] as $req) {
-			if (in_array($req, array('jumio'))) {
-				$has_kyc = false;
-				break 2;
-			}
-		}
-		if ($buy_limit['total']['currency'] != 'USD') {
-			$foreign_currency = $buy_limit['total']['currency'];
-			continue;
-		}
-		$avg_daily_buy_limit = $buy_limit['total']['amount'] / $buy_limit['period_in_days'];
-		if ($avg_daily_buy_limit > 500) {
-			$has_kyc = true;
-		}
-// 		echo("Buy limit: $avg_daily_buy_limit<br>");
-    }
-    if ($has_kyc) {
-		echo("KYC is OK!");
-    } else if ($foreign_currency) {
-		echo("Unknown currency $foreign_currency");
-    } else if (!$found_bank) {
-		echo("No funding source found");
-    } else {
-		echo("KYC not completed");
-    }
-}
 ?>
