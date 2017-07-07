051ea870334b6d72c42af13a43c55d5469a39c6d
Luke Dashjr <luke-jr+git+jun@utopios.org>
2017-07-07 04:22:20 +0000

    Move more to kycpoll.php

--- coinbase.php
+++ coinbase.php
@@ -5,11 +5,7 @@ $i_am_not_direct = true;
 require_once('secrets.php');
 require('kycpoll.php');
 
-echo('<!DOCTYPE html>');
-echo("<html lang='en'><head><title>KYCPoll</title>");
-echo('<link rel="stylesheet" type="text/css" href="style.css">');
-echo('<script src="save.js" type="text/javascript"></script>');
-echo("</head><body>");
+pageheader();
 
 function myerr($msg) {
 	echo("Error: $msg</body></html>");
@@ -92,20 +88,14 @@ echo('<div id="save_button_placeholder"></div>');
 echo("Hello ".$userdata['coinbase_userdata']['data']['name']."<br>");
 echo('<br>');
 
-echo('This data will be saved with your poll results. If there is too much personal information included, please contact luke-jr to improve the filtering <em>before</em> filling out the poll.<br>');
-echo('Note that if you have not completed KYC with Coinbase, your results will be saved but ignored until you complete KYC <em>and</em> resubmit your poll results.<br>');
-
-echo('<textarea readonly style="width:100%" rows="15">' . htmlentities(json_encode($userdata, JSON_PRETTY_PRINT), ENT_HTML5 | ENT_NOQUOTES) . '</textarea>');
-echo('<form action="#" method="post" id="pollform">');
+datadisclosure();
+formbegin();
 echo('<input type="submit" name="userdata_refresh" value="Refresh data from Coinbase"><br>');
-// echo('<input type="hidden" name="ABC" value="DEF" id="ABC"><input type="submit"></form>');
-// echo('<pre>');var_dump($_POST);echo('</pre>');
-echo('<input id="accept_terms" name="accept_terms" type="checkbox" onclick="accept_terms_clicked()"' . (@$_POST['accept_terms'] ? ' checked' : '') . '><label for="accept_terms">I agree that the server may save this data, and that there is no promise of this data being kept secure</label><br>');
+datadisclosure_checkbox();
 
 polls();
 
-echo("</form>");
-echo("<br><br>(the save button is on the top-right corner of your window)");
-echo("</body></html>");
+formend();
+pagefooter();
 
 ?>
