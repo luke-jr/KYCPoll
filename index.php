30ecd7ecb202bdf8e0c12d75d9bbd02268eafb6e
Luke Dashjr <luke-jr+git@utopios.org>
2017-07-11 09:27:27 +0000

    Link the answers

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ echo("<h1>Hello!</h1>");
 echo("<p>To access KYCPoll, please pass the KYC process to ensure you are a real person.</p>");
 echo("<p>KYCPoll currently only supports Coinbase for KYC.</p>");
 
-echo("<a class='btn' href='coinbase.php'>Click here to continue</a>");
+echo("<a class='btn' href='coinbase.php'>Click here to take the poll</a>");
+
+echo("<a class='btn' href='answers.php'>Click here to see poll answers</a>");
+
 echo("</div>");
 
 pagefooter();
