5741b13636b7c1b04f893b70b2cf4314aa9d2807
Luke Dashjr <luke-jr+git@utopios.org>
2017-07-11 08:11:33 +0000

    Move save.js until we actually include the checkbox

--- htmlstuff.php
+++ htmlstuff.php
@@ -8,7 +8,6 @@ function pageheader() {
 	echo('<!DOCTYPE html>');
 	echo("<html lang='en'><head><title>KYCPoll</title>");
 	echo('<link rel="stylesheet" type="text/css" href="style.css">');
-	echo('<script src="save.js" type="text/javascript"></script>');
 	echo("</head><body>");
 	echo('<a href="https://github.com/luke-jr/KYCPoll/" style="position: absolute; top: 0; left: 0; border: 0;"><img src="https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"></a>');
 }
@@ -26,6 +25,7 @@ function formbegin() {
 }
 
 function datadisclosure_checkbox() {
+	echo('<script src="save.js" type="text/javascript"></script>');
 	echo('<input id="accept_terms" name="accept_terms" type="checkbox" onclick="accept_terms_clicked()"' . (@$_POST['accept_terms'] ? ' checked' : '') . '><label for="accept_terms">I agree that the server may save this data, and that there is no promise of this data being kept secure</label><br>');
 }
 
